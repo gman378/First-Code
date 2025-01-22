4222dc4f443a470b06c22d07ef82908de2ce5bc4
Derek Moreira <111708887+gman378@users.noreply.github.com>
2025-01-22 17:54:53 -0500

    Teste de outras variaveis

---
+++ TesteVariaveis.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int Numero;
+	Numero = 45;
+	std::cout << "Valor do Numero: " << Numero << std::endl;
+	std::cout << "Tamanho do numero: " << sizeof(Numero) << "\n";
+	std::cout << "endereso carregado  na memoria: " << &Numero << "\n";
+	system("PAUSE");
+
+
+
+}
