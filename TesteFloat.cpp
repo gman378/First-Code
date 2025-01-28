54a4665c19b1994d1de2eb867d18df9cd3321eec
Derek Moreira <111708887+gman378@users.noreply.github.com>
2025-01-27 23:22:30 -0500

    Add files via upload
    Teste de Float e Double.

---
+++ TesteFloat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	float Numero;
+	double Numero2 = 45345.904555;
+
+	Numero = 55.56f;
+
+	std::cout << "Quanto tem o numero?: " << Numero << std::endl;
+	
+	std::cout << "Quantos numeros tem no double?: " << Numero2 << std::endl;
+
+	system("PAUSE");
+	return 0;
+
+}
