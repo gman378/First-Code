664863c518574e18f20f0f8d9e9fdb328a082f94
Derek Moreira <111708887+gman378@users.noreply.github.com>
2025-02-21 21:38:53 -0500

    Teste de Bool.
    Teste de Bool.

---
+++ TesteBooleano.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+
+	bool bAchou;
+
+	bAchou = 1;
+
+	std::cout << "Qual valor Bachou?: " << bAchou << std::endl;
+
+	bAchou = 0;
+
+	std::cout << "Qual agora o valor de Bachou?: " << bAchou << std::endl;
+
+	bAchou = true;
+
+	std::cout << "Qual agora o valor de Bachou?: " << bAchou << std::endl;
+
+	bAchou = false;
+
+	std::cout << "Qual agora o valor de Bachou?: " << bAchou << std::endl;
+
+	bAchou = 'D';
+
+	std::cout << "Qual agora o valor de Bachou?: " << bAchou << std::endl;
+
+	system("PAUSE");
+	return 0;
+
+	//Qualquer coisa que seja acima de 0, é identificado como True.
+
+}
