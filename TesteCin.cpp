f0eb58eb59feb81783e9b299b7a66e7867464839
Derek Moreira <111708887+gman378@users.noreply.github.com>
2025-02-24 19:23:08 -0500

    Teste usando CIN

---
+++ TesteCin.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+
+	int Numero01, Numero02;
+
+	std::cout << "Digite o primeiro numero: ";
+	std::cin >> Numero01;
+	std::cout << "Numero01 = " << Numero01 << std::endl;
+	
+	std::cout << "Digite o segundo numero; ";
+	std::cin >> Numero02;
+	std::cout << "Numero02 = " << Numero02 << std::endl;
+	
+	
+	system("PAUSE");
+
+
+}
