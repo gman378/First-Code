043dd517928ec073949e01adbe468c44254e82f2
Derek Moreira <111708887+gman378@users.noreply.github.com>
2025-02-24 21:00:16 -0500

    Add files via upload

---
+++ TesteCinCalculadora.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+
+		int Num1, Num2, Num3, Num4;
+
+		std::cout << "Digite Quatro numeros separados: ";
+
+		std::cin >> Num1 >> Num2 >> Num3 >> Num4;
+
+		std::cout << "Soma = " << (Num1 + Num2 + Num3 + Num4) << std::endl;
+
+		return 0;
+
+}
