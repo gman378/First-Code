58ddc4b221fe40c3f7517b7d56ddd36e9ce2411a
Derek Moreira <111708887+gman378@users.noreply.github.com>
2025-03-02 14:14:48 -0500

    Terceiro teste do comando "Cin".

---
+++ TesteCin3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+
+{
+
+	int Num1, Num2;
+	int Resultado;
+	
+	std::cout << "Digite seu primeiro numero: ";
+	std::cin >> Num1;
+	std::cout << "Numero 1: " << Num1 << std::endl;
+	std::cout << "Digite o segundo numero: ";
+	std::cin >> Num2;
+	std::cout << "Numero 2: " << Num2 << std::endl;
+	Resultado = Num1 * Num2;
+	std::cout << "O resultado dos dois numeros: " << Resultado << std::endl;
+
+	system("PAUSE");
+
+
+
+
+}
