71ca7a75634e5baa351fafd61b1f33f1253c775c
Derek Moreira <111708887+gman378@users.noreply.github.com>
2025-05-27 00:39:55 -0500

    Teste de Login.
    Não finalizado.

---
+++ Avançados/login.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+	std::string username, senha, RespostaCerta;
+	std::string username2, senha2, RespostaCerta2;
+	char Autenticaçao;
+
+	std::cout << "Crie um um nome de usuario: ";
+	std::cin >> username;	
+	std::cout << "Crie uma senha: ";
+	std::cin >> senha;
+
+	RespostaCerta == (senha, username);
+
+	std::cout << "Faça login, informe seu username: ";
+	std::cin >> username2;	
+	std::cout << "Informe sua senha: ";
+	std::cin >> senha2;
+
+	RespostaCerta2 == (senha2, username2);	
+
+	if (RespostaCerta == RespostaCerta2)
+	{
+		std::cout << "Login realizado com sucesso!" << std::endl;
+		return 0;
+	}
+	else if (RespostaCerta != RespostaCerta2)
+	{ 
+	
+		std::cout << "Login ou senha incorretos" << std::endl;
+		return 0;
+
+	}
+
+
+
+	return 0;
+}
