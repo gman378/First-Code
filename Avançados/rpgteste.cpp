6f4cc8baafcaa1d56e483368a516eeb368100db1
Derek Moreira <111708887+gman378@users.noreply.github.com>
2025-05-27 21:33:35 -0500

    Update rpgteste.cpp

--- Avançados/rpgteste.cpp
+++ Avançados/rpgteste.cpp
@@ -3,12 +3,17 @@
 
 int main()
 {
+       //Rpg simples de texto, ainda em desenvolvimento.
+	
 	setlocale(LC_ALL, "portuguese");
 
 	int Vida = 100;
 	int Ataque = 10;
+	int Mana = 0;
+        int Defesa = 0;
+	//int Stamina = 0;
 	std::string Nome;
-	std::string Raça;
+	std::string RaÃ§a;
 	std::string Classe;
 	int EscolhaRaca;
 	int EscolhaClasse;
@@ -19,7 +24,7 @@ int main()
 	std::cin >> Nome;
 
 
-	std::cout << "|Escolha sua raça.|" << "\n";
+	std::cout << "|Escolha sua raÃ§a.|" << "\n";
 	std::cout << "1 - Humano " << "\n";
 	std::cout << "2 - Elfo " << "\n";
 	std::cout << "3 - Orc " << "\n";
@@ -28,35 +33,38 @@ int main()
 
 	if (EscolhaRaca == 1)
 	{
-		Raça = "Humano";
+		RaÃ§a = "Humano";
 		Ataque += 5;
+		Defesa += 15;
 
 		//Bonus de ataque bruto para humano.
 	}
 	else if (EscolhaRaca == 2)
 	{
-		Raça = "Elfo";
+		RaÃ§a = "Elfo";
 		Vida += 30;
+		Defesa += 5;
 
 		//Bonus de vida para Elfo.
 	}
 	else if (EscolhaRaca == 3)
 	{
-		Raça = "Orc";
+		RaÃ§a = "Orc";
 		Vida += 10;
-		Ataque += 10;	
+		Ataque += 10;
+		Defesa += 10;
 
 		//Bonus de vida e ataque para Orc.	
 	}
 	else
 	{
-		Raça = "Humano";
-		std::cout << "|Raça invalida, voce sera um Humano por padrão.|" << "\n";
+		RaÃ§a = "Humano";
+		std::cout << "|RaÃ§a invalida, voce sera um Humano por padrÃ£o.|" << "\n";
 	}
 
-	//Aqui o jogador escolhe uma raça. e recebe um bonus dependendo da raça escolhida.
+	//Aqui o jogador escolhe uma raÃ§a. e recebe um bonus dependendo da raÃ§a escolhida.
 
-	std::cout << "|Voce escolheu a raça: " << Raça << "|" << "\n";
+	std::cout << "|Voce escolheu a raÃ§a: " << RaÃ§a << "|" << "\n";
 	std::cout << "|Vida: " << Vida << "| Ataque: " << Ataque << " |" << "\n";
 
 	//Escolher classes.
@@ -73,12 +81,14 @@ int main()
 		Classe = "Guerreiro";
 		Vida += 10;
 		Ataque += 10;
+		Mana += 10;
 	}
 	else if (EscolhaClasse == 2)
 	{
 		Classe = "Mago";
 		Vida += 15;
 		Ataque += 5;
+		Mana += 100;
 	}
 	else if (EscolhaClasse == 3)
 	{
@@ -86,21 +96,22 @@ int main()
 		Classe = "Arqueiro";
 		Vida += 5;
 		Ataque += 15;
+		Mana += 10;
 	
 	}
 	else
 	{
 		Classe = "Guerreiro";
-		std::cout << "|Classe invalida, voce sera um Guerreiro por padrão.|" << "\n";	
+		std::cout << "|Classe invalida, voce sera um Guerreiro por padrÃ£o.|" << "\n";	
 
 	}
 
 	std::cout << "|Voce escolheu a classe: " << Classe << "|" << "\n";
-	std::cout << "|Vida: " << Vida << "| Ataque: " << Ataque << " |" << "\n";
 
 	std::cout << "|Ficha do personagem|" << "\n";
 	std::cout << "|Nome: " << Nome << "|" << "\n";
-	std::cout << "|Raça: " << Raça << "|" << "\n";
+	std::cout << "|RaÃ§a: " << RaÃ§a << "|" << "\n";
+	std::cout << "|Vida: " << Vida << "| Ataque: " << Ataque << "| Mana: " << Mana << "|" << "\n";
 
 	return 0;
 }
